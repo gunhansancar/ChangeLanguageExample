e7fda3558dc4573690834d0933a5177a56834773
Gunhan Sancar <Gunhan.Sancar@thisisglobal.com>
2016-12-30 23:16:53 +0000

    Main application updated

--- app/src/main/java/com/gunhansancar/changelanguageexample/MainApplication.java
+++ app/src/main/java/com/gunhansancar/changelanguageexample/MainApplication.java
@@ -5,16 +5,7 @@ import android.content.Context;
 
 import com.gunhansancar.changelanguageexample.helper.LocaleHelper;
 
-/**
- * Created by gunhan.sancar on 20/12/2016.
- */
-
 public class MainApplication extends Application {
-	@Override
-	public void onCreate() {
-		super.onCreate();
-	}
-
 	@Override
 	protected void attachBaseContext(Context base) {
 		super.attachBaseContext(LocaleHelper.onAttach(base, "en"));
